9624083fcbcbf99699ef7c7f5907a1060088f06f
NevenaPetro <a.nevena.petrovic@gmail.com>
2023-05-24 15:16:31 +0200

    homepage

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as BoltLogo } from "../../assets/footer/bolt-logo.svg";
 import ThemeIcon from "../../assets/home/theme-icon.svg";
 import "../header/header.scss";
 
 function Header() {
+  const [active, setActive] = useState(false);
+  const menuIcon = "menuIcon";
   return (
     <div className="header-wraper">
       <Link className="logo" to="/">
         <BoltLogo />
       </Link>
       <div className="menu">
-        <nav className="navigation">
+        <nav className={`navigation ${active ? menuIcon : ""}`}>
           <Link to="/generate">Generate QR Codes</Link>
           <Link to="/pricing">Pricing</Link>
           <Link to="/login">Log in</Link>
-          <Link to="/signup" className="transp-btn">Sign up</Link>
+          <Link to="/signup" className="transp-btn">
+            Sign up
+          </Link>
         </nav>
         <button className="theme-btn">
           <img src={ThemeIcon} alt="theme" />
         </button>
+        <div
+          className={`hamburger ${active ? menuIcon : ""}`}
+          onClick={() => {
+            setActive(!active);
+          }}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
       </div>
     </div>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import QRCodePurchase from "../../assets/home/qr-code-purchase.png";
+import "../HomePage/homepage.scss";
 
 function HomePage() {
   return (
-    <div>
-      <h2>Home Page</h2>
+    <div className="homepage-wraper">
+      <div>
+        <div className="heading">
+          <h2>
+            Strikingly fast <span>QR codes.</span>
+          </h2>
+
+          <p>You are one Bolt away from never being too slow again.</p>
+        </div>
+        <div className="big-btns">
+          <div className="big-btn">
+            <Link to="/generate">Generate QR Codes</Link>
+          </div>
+          <div className="big-btn">
+            <Link to="/signup">Sign up</Link>
+          </div>
+        </div>
+      </div>
+      <img src={QRCodePurchase} alt="image" />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
