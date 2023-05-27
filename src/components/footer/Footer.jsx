0eb49302f0d57b52136229ac771011866d3697f7
NevenaPetro <a.nevena.petrovic@gmail.com>
2023-05-27 13:45:04 +0200

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,28 +15,55 @@ function Footer() {
   return (
     <div className="footer-wrapper">
       <div className="footer-sub-wrapper">
-        <div>
-          <img src={TarabaLogo} alt="logo" />
-          <img src={LinkedinIcon} alt="linkedin" />
-          <img src={InstagramIcon} alt="instagram" />
-          <img src={YouTubeIcon} alt="youtube" />
-          <img src={FacebookIcon} alt="facebook" />
-          <p>Copyright © 2021 Taraba. All rights reserved.</p>{" "}
+        <div className="footer-sub-sub-wrapper">
+          <div>
+            <img src={TarabaLogo} alt="logo" className="tarabaLogo" />
+            <div className="social-networks">
+              <Link>
+                <img src={LinkedinIcon} alt="linkedin" />
+              </Link>
+              <Link>
+                <img src={InstagramIcon} alt="instagram" />
+              </Link>
+              <Link>
+                <img src={YouTubeIcon} alt="youtube" />
+              </Link>
+              <Link>
+                <img src={FacebookIcon} alt="facebook" />
+              </Link>
+            </div>
+          </div>
+          <p className="copyright">Copyright © 2021 Taraba. All rights reserved.</p>{" "}
         </div>
-        <div>
-            <h6>Quick links</h6>
+        <div className="footer-sub-sub-wrapper">
+          <h6>Quick links</h6>
+          <div className="quick-links">
             <Link>Generate QR codes</Link>
             <Link>Pricing</Link>
+          </div>
+          <div>
             <Link>Terms and Conditions</Link>
+            <span> </span>
             <Link>Privacy Policy</Link>
+          </div>
         </div>
       </div>
       <div className="footer-wrapper-grey">
         <p>Our family</p>
-        <Link><img src={TechLogo} alt="tech" /></Link>
-        <Link><img src={EduLogo} alt="edu" /></Link>
-        <Link><img src={PortalLogo} alt="portal" /></Link>
-        <Link><img src={OaklyIcon} alt="oakly" /></Link>
+        <div className="family-links">
+          <Link>
+            <img src={TechLogo} alt="tech" />
+          </Link>
+          <Link>
+            <img src={EduLogo} alt="edu" />
+          </Link>
+          <Link>
+            <img src={PortalLogo} alt="portal" />
+          </Link>
+          <Link>
+            <img src={OaklyIcon} alt="oakly" />
+          </Link>
+        </div>
       </div>
     </div>
   );
